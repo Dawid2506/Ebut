ee7fd75f00a47353e02039bcebc542804a8eca9b
Dawid Tkacz <104102596+Dawid2506@users.noreply.github.com>
2024-03-05 14:31:52 +0100

    test

--- src/components/specificItemPage/SpecificInformation.js
+++ src/components/specificItemPage/SpecificInformation.js
@@ -11,7 +11,7 @@ const SpecificInformation = ({toggleBlur}) => {
         <div className='specificInfoContainer'>
             <h2 className='specBrand'>adidas</h2>
             <p className='specDescription'>Buty Runfalcon 2.0 I GX3540 Dark Blue/Cloud White/Blue Rush</p>
-            <h1 className='specPrice'>139,99 zł</h1>
+            <h1 className='specPrice'>138,99 zł</h1>
             <div className='sizeTable' onClick={setCheck}>
                 <p>Tabela rozmiarów</p>
             </div>
